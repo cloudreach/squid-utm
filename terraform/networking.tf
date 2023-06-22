8eb259170aa1f38cd1594e3485142b9ac4eb923d
Cyril Feraudet <cyril@feraudet.com>
2023-06-22 11:36:10 +0200

    Restrict SG

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "fargate" {
     from_port   = var.app_port
     to_port     = var.app_port
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
